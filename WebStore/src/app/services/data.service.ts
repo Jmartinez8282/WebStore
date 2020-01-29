6893dd35585c93eb22edd44e2926b8fc4ec53832
jmartinez8282 <josemartinez8282@gmail.com>
2020-01-28 22:34:42 -0800

    cp

--- WebStore/src/app/services/data.service.ts
+++ WebStore/src/app/services/data.service.ts
@@ -1,61 +1,66 @@
 import { Injectable } from '@angular/core';
 import { Iproduct } from '../interfaces/iproduct';
-import { Observable,of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-private products: Iproduct[]=[
-  {
-    id: '1',
-    pName: 'Ninja Stars',
-    pNameURL: 'NinjaStars',
-    pDesc: 'Something to do with Ninja Stars',
-    additionalInfo: 'They can cause a paper cut',
-    price: '$1.00'
-  },
-  {
-    id: '2',
-    pName: 'Ninja Starzzzz',
-    pNameURL: 'NinjaStars',
-    pDesc: 'Something to do with Ninja Stars',
-    additionalInfo: 'They can cause a paper cut',
-    price: 'Free'
-  },
-  {
-    id: '3',
-    pName: 'Ninja Starszzzzzzzzzzz',
-    pNameURL: 'NinjaStars',
-    pDesc: 'Something to do with Ninja Stars',
-    additionalInfo: 'They can cause a paper cut',
-    price: '$1111.00'
-  },
-  {
-    id: '4',
-    pName: 'Ninja Starszzzzzzzzzzzzzzzz',
-    pNameURL: 'NinjaStars',
-    pDesc: 'Something to do with Ninja Stars',
-    additionalInfo: 'They can cause a paper cut',
-    price: '$111.00'
-  },
-  {
-    id: '5',
-    pName: 'Ninja ',
-    pNameURL: 'NinjaStars',
-    pDesc: 'Something to do with Ninja Stars',
-    additionalInfo: 'They can cause a paper cut',
-    price: '$11.00'
-  }
-]
+  private products: Iproduct[] = [
+    {
+      id: '1',
+      pName: 'CyberTruck T-Shirt',
+      pNameURL: 'CyberTruck T',
+      pDesc: 'Black soft Cotton t-shirt image of cyber truck broken glass',
+      additionalInfo: 'image displays shatter glass of cyber truck',
+      price: '$11.00',
+      imageURL: "../../assets/Images/ImageX1.png"
+    },
+    {
+      id: '2',
+      pName: 'Ninja Starzzzz',
+      pNameURL: 'NinjaStars',
+      pDesc: 'Something to do with Ninja Stars',
+      additionalInfo: 'They can cause a paper cut',
+      price: 'Free',
+      imageURL: '../../assets/Images/ImageX1.png'
+    },
+    {
+      id: '3',
+      pName: 'Ninja Starszzzzzzzzzzz',
+      pNameURL: 'NinjaStars',
+      pDesc: 'Something to do with Ninja Stars',
+      additionalInfo: 'They can cause a paper cut',
+      price: '$1111.00',
+      imageURL: '../../assets/Images/ImageX1.png'
+    },
+    {
+      id: '4',
+      pName: 'Ninja Starszzzzzzzzzzzzzzzz',
+      pNameURL: 'NinjaStars',
+      pDesc: 'Something to do with Ninja Stars',
+      additionalInfo: 'They can cause a paper cut',
+      price: '$111.00',
+      imageURL: '../../assets/Images/ImageX1.png'
+    },
+    {
+      id: '5',
+      pName: 'Ninja ',
+      pNameURL: 'NinjaStars',
+      pDesc: 'Something to do with Ninja Stars',
+      additionalInfo: 'They can cause a paper cut',
+      price: '$11.00',
+      imageURL: '../../assets/Images/ImageX1.png'
+    }
+  ]
   constructor() { }
-  getProducts():Iproduct []{
-  
+  getProducts(): Iproduct[] {
+
     return this.products;
   }
-  getProduct(sku: string):Observable<Iproduct>{
+  getProduct(sku: string): Observable<Iproduct> {
     return of(
-    this.products.find(
-      x => x.id === sku
-    ));
+      this.products.find(
+        x => x.id === sku
+      ));
   }
 }
