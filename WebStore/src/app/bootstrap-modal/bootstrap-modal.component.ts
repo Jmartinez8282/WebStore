779608e71ca9edce0b50be757a515fad3c5357d0
Code <code@COCTL-41256.local>
2020-02-05 16:14:39 -0800

    cp

--- WebStore/src/app/bootstrap-modal/bootstrap-modal.component.ts
+++ WebStore/src/app/bootstrap-modal/bootstrap-modal.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { DataService } from '../services/data.service';
+import { Iproduct } from '../interfaces/iproduct';
 @Component({
   selector: 'app-bootstrap-modal',
   templateUrl: './bootstrap-modal.component.html',
   styleUrls: ['./bootstrap-modal.component.scss']
 })
 export class BootstrapModalComponent implements OnInit {
+  productList: Iproduct[];
   closeResult: string;
   @Input() product;
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal,private dService:DataService) { }
 
   ngOnInit() {
+    this.productList = this.dService.getProducts();
   }
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', size: 'xl'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
